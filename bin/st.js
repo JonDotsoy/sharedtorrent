502e1c1a64977cc598f55c4040fb865c91f7cd71
Jonathan Delgado Zamorano <hi@jon.soy>
2016-11-05 17:19:13 -0300

    Muchas cosas nuevas? :D Pero ya se puede usar.

--- bin/st.js
+++ bin/st.js
@@ -103,12 +103,74 @@ const actionNoWork = () => {
 	log(`${colors.yellow("Sorry No Working")}`);
 }
 
+const actionCreateRSS = (SET_OUTFILE) => {
+	log(colors.yellow(`Create RSS.`));
+
+	let e = sharedtorrent.from(preloadSharedTorrent());
+	const OUTFILE = path.resolve(SET_OUTFILE || `${e.body.title.replace(/\s/g,".")}.xml`);
+
+	// makeSharedTorrent(e.toXML());
+
+	log("Update file " + colors.green(OUTFILE) + ":");
+	// fileWriteSync()
+	// console.log()
+	//
+
+	fs.writeFileSync(OUTFILE, e.toXML());
+}
+
+const actionCreateMarkdown = (SET_OUTFILE) => {
+	log(colors.yellow(`Create Markdown file.`));
+
+	let e = sharedtorrent.from(preloadSharedTorrent());
+	const OUTFILE = path.resolve(SET_OUTFILE || `${e.body.title.replace(/\s/g,".")}.md`);
+
+	// makeSharedTorrent(e.toXML());
+
+	log("Update file " + colors.green(OUTFILE) + ":");
+
+	fs.writeFileSync(OUTFILE, e.toMD());
+}
+
+const actionImport = (filetoimport) => {
+	log(colors.yellow(`Import MAGENTS links.`));
+	let f = fs.readFileSync(filetoimport).toString().split("\n");
+
+	console.log( JSON.stringify(f, null, 2) );
+
+	let e = sharedtorrent.from(preloadSharedTorrent());
+
+	for (var i = f.length - 1; i >= 0; i--) {
+		e.addTorrent(f[i]);
+	}
+
+	makeSharedTorrent(e.toString());
+
+	log("update file " + colors.green(FILE_SHAREDTORRENT) + ":");
+	log(e.toString());
+}
+
+const actionList = () => {
+	log(colors.yellow(`List Titles.`));
+
+	let e = sharedtorrent.from(preloadSharedTorrent());
+
+	log(e.body.torrents.map(t=>t.name).join("\n"));
+}
+
 info();
 switch (String(argv["_"][0]).toLowerCase()) {
 case "init": actionInit(); break;
 case "inspect": actionInspect(); break;
 case "add": actionAddTorrent(argv["_"][1]); break;
 case "set": actionSetProp(String(argv["_"][1]).toLowerCase(), argv["_"].slice(2).join(" ")); break;
+case "xml": actionCreateRSS(argv["_"][1]); break;
+case "rss": actionCreateRSS(argv["_"][1]); break;
+case "md":
+case "markdown": actionCreateMarkdown(argv["_"][1]); break;
+case "import": actionImport(argv["_"][1]); break;
+case "ls":
+case "list": actionList(); break;
 default: actionNoWork(); break;
 }
 
